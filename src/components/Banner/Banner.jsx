2948da2a145b67f4445b8dcb85b12f690286d888
Gulzhan Karakul <karakulgulzhan@gmail.com>
2024-02-21 17:30:10 +0600

    комментарии к анимации текста(banner)2

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,50 +4,48 @@ import { useEffect, useState } from "react";
 import './Banner.css'
 
 export default function Banner() {
-    // Инициализация состояний компонента
-    const [loopNum, setLoopNum] = useState(0); // Счетчик итераций
-    const [isDeleting, setIsDeleting] = useState(false); // Флаг для определения, удаляется ли текст
-    const toRotate = ["Web Developer", "JavaScript developer"]; // Массив строк для смены
-    const [text, setText] = useState(''); // Текущий отображаемый текст
+    const [loopNum, setLoopNum] = useState(0);                    // Счетчик итераций
+    const [isDeleting, setIsDeleting] = useState(false);          // Флаг для определения, удаляется ли текст
+    const toRotate = ["Web Developer", "JavaScript developer"];   // Массив строк для смены
+    const [text, setText] = useState('');                         // Текущий отображаемый текст
     const [delta, setDelta] = useState(300 - Math.random() * 100); // Время задержки между символами
-    const period = 2000; // Период между сменой строк
+    const period = 2000;                                           // Период между сменой строк
 
-    // Хук useEffect позволяет выполнять побочные эффекты в функциональных компонентах. 
-    // Побочные эффекты включают в себя такие вещи, как изменение DOM, запросы к серверу, подписка на события и т. д.
-    // Он выполняется после каждого рендера компонента.
-    // Запуск эффекта при изменении текста
     useEffect(() => {
         let ticker = setInterval(() => {
-            tick(); // Вызов функции tick для обновления текста
-        }, delta); // Задержка между вызовами функции tick
+            tick();      // Вызов функции tick для обновления текста
+        }, delta);       // Задержка между вызовами функции tick
 
         // Очистка интервала при размонтировании компонента
         return () => { clearInterval(ticker)};
-    }, [text]); // Эффект запускается при изменении текста
-
+    }, [text]);  // Эффект запускается при изменении текста
+ 
     // Функция для обновления текста
     const tick = () => {
-        let i = loopNum % toRotate.length; // Определение текущей строки для отображения
-        let fullText = toRotate[i]; // Получение полного текста для текущей строки
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1); // Обновление текста в зависимости от флага isDeleting
+        let i = loopNum % toRotate.length;    // Определение текущей строки для отображения
+        let fullText = toRotate[i];           // Получение полного текста для текущей строки
+        // Обновление текста в зависимости от флага isDeleting
+        // fullText.substring(0, text.length + 1) - к updatedText по букве добавляется слово
+        // fullText.substring(0, text.length - 1) - updatedText по буквам удаляется, 
+        // пока setDelta не срабатывает и время на удаления текста уменьшается и текст быстрее удаляется
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1); 
 
-        setText(updatedText); // Установка нового текста
+        setText(updatedText);                 // Установка нового текста
 
         // Управление временем задержки между символами в зависимости от удаления текста
         if (isDeleting) setDelta(prevDelta => prevDelta / 2);
         
         // Условие для смены направления изменения текста
         if (!isDeleting && updatedText === fullText) {
-            setIsDeleting(true); // Установка флага удаления текста
-            setDelta(period); // Установка задержки на период
+            setIsDeleting(true);           // Установка флага удаления текста
+            setDelta(period);              // Установка задержки на период
         } else if (isDeleting && updatedText === '') {
-            setIsDeleting(false); // Сброс флага удаления текста
-            setLoopNum(loopNum + 1); // Увеличение счетчика итераций
-            setDelta(500); // Установка времени задержки между символами
+            setIsDeleting(false);           // Сброс флага удаления текста
+            setLoopNum(loopNum + 1);        // Увеличение счетчика итераций
+            setDelta(500);                  // Установка времени задержки между символами
         }
     }
 
-    // Возвращение разметки компонента
     return (
         <section className="banner" id="home">
             <Container>
