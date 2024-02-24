42a2b66f06211995f99298670a685f8136b446e4
Gulzhan Karakul <karakulgulzhan@gmail.com>
2024-02-24 23:23:51 +0600

    adding my projects utils

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,17 +2,33 @@ import projImg1 from "../assets/img/port1.jpg";
 import projImg2 from "../assets/img/port2.jpg";
 import projImg3 from "../assets/img/port5.jpg";
 import jsProjectImg1 from '../assets/img/commercial-project1.png'
-import portfolioProjectImg1 from '../assets/img/my-portfolioImg.png'
+import jsProjectImg2 from '../assets/img/coffee-projectImg.png'
+import jsProjectImg3 from '../assets/img/test-projectImg.png'
+import reactProjectImg2 from '../assets/img/my-portfolioImg.png'
 
 export const projects = {
     vanillaJS: [
         {
-            title: "Html, CSS and Vanilla Js Project",
+            title: "My Commercial Vanilla Js Project",
             description: "Design & Development",
             imgUrl: jsProjectImg1,
-            gitUrl: 'https://github.com/GulzhanKarakul/equipment_repair_website/settings',
+            gitUrl: 'https://github.com/GulzhanKarakul/equipment_repair_website',
             webUrl: 'https://icecoldservice.netlify.app/',
-        }
+        },
+        {
+            title: "My Pet Project using State Machine",
+            description: "Design & Development",
+            imgUrl: jsProjectImg2,
+            gitUrl: 'https://github.com/GulzhanKarakul/coffee-machine',
+            webUrl: 'https://gulzhankarakul.github.io/coffee-machine/',
+        },
+        {
+            title: "My Mini-Pet Project using LocalStorage",
+            description: "Design & Development",
+            imgUrl: jsProjectImg3,
+            gitUrl: 'https://github.com/GulzhanKarakul/test-project',
+            webUrl: 'https://gulzhankarakul.github.io/test-project/',
+        },
     ],
     react: [
         {
@@ -25,7 +41,7 @@ export const projects = {
         {
             title: "My beautiful Portfolio Site",
             description: "Design & Development",
-            imgUrl: portfolioProjectImg1,
+            imgUrl: reactProjectImg2,
             gitUrl: 'https://github.com/GulzhanKarakul/my-portfolio',
             webUrl: 'https://gulzhankarakul.github.io/my-portfolio/',
         },
